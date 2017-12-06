f67f5eaf5dc929fc2c78ef820413666f20e2bcce
l-gu <laurent.guer1@gmail.com>
2017-12-06 14:55:36 +0100

    Minor changes

--- src/main/java/td/servlet/td7/Cadre.java
+++ src/main/java/td/servlet/td7/Cadre.java
@@ -20,20 +20,17 @@ public class Cadre extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
 
-	public void init() throws ServletException
-    {
+	public void init() throws ServletException {
         System.out.println("init : Servlet Cadre ");
     }
 
     //--- Requete GET
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         process(request, response);
     }
 
     //--- Requete POST
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         process(request, response);
     }
 
@@ -60,9 +57,30 @@ public class Cadre extends HttpServlet
         out.println("</body>");
         out.println("</html>");
     }
+    
+    private void includeResource(String uri, HttpServletRequest request, HttpServletResponse response) 
+    	throws ServletException, IOException {
+        if ( uri != null) {
+            //--- Passage d'objet par la requête
+            request.setAttribute("param", uri); // Stockage de l'objet en tant qu'attribut de requête
 
-    private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
+            //--- Inclusion du contenu 
+            RequestDispatcher rd = request.getRequestDispatcher(uri);
+            if (rd != null) {
+                System.out.println("Avant INCLUDE " + uri );
+                rd.include(request, response);
+                System.out.println("Après INCLUDE");
+            }
+            else {
+                throw new RuntimeException("Cannot get RequestDispatcher");
+            }
+        }
+        else {
+            throw new IllegalArgumentException("Invalid URI");
+        }
+    }
+    
+    private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String ressourceAInclure = null;
 
         response.setContentType("text/html");
@@ -72,47 +90,54 @@ public class Cadre extends HttpServlet
         header ( out );
         
         //--- Récupération du nom ( paramètre de la requête )
-        String paramPage = request.getParameter("page");
-        if (paramPage == null)
-        {
-            out.println("<h2>Erreur : Pas de numéro de page ! </h2>");
-        }
-        else
-        {
+        String paramContenu = request.getParameter("contenu");
+        if (paramContenu != null) {
 
-            if (paramPage.equals("1"))
-            {               
+            if (paramContenu.equals("1")) {               
                 ressourceAInclure = "/Contenu1"; // URL mappée sur la Servlet "Contenu"
             }
-            else if (paramPage.equals("2"))
-            {
+            else if (paramContenu.equals("2")) {
                 ressourceAInclure = "/Contenu2"; // Autre URL également mappée sur la Servlet "Contenu"
-            }
-
-            if (ressourceAInclure != null)
-            {
-                //--- Passage d'objet par la requête
-                request.setAttribute("param", ressourceAInclure); // Stockage de l'objet en tant qu'attribut de requête
-
-                //--- Inclusion du contenu 
-                RequestDispatcher rd = request.getRequestDispatcher(ressourceAInclure);
-                if (rd != null)
-                {
-                    System.out.println("Avant INCLUDE " + ressourceAInclure );
-                    rd.include(request, response);
-                    System.out.println("Après INCLUDE");
-                }
-                else
-                {
-                    out.println("<h2>Erreur : Request Dispatcher = null ! </h2>");
-                }
-            }
-            else
-            {
-                out.println("<h2>Erreur : Numéro de page incorrect ! </h2>");
             }
         }
 
+        String part = request.getParameter("part");
+        if (part != null) {
+            if (part.equals("1")) {
+                ressourceAInclure = "/part1"; 
+            }
+            else if (part.equals("2")) {
+                ressourceAInclure = "/part2"; 
+            }
+        }
+//            if (ressourceAInclure != null) {
+//                //--- Passage d'objet par la requête
+//                request.setAttribute("param", ressourceAInclure); // Stockage de l'objet en tant qu'attribut de requête
+//
+//                //--- Inclusion du contenu 
+//                RequestDispatcher rd = request.getRequestDispatcher(ressourceAInclure);
+//                if (rd != null)
+//                {
+//                    System.out.println("Avant INCLUDE " + ressourceAInclure );
+//                    rd.include(request, response);
+//                    System.out.println("Après INCLUDE");
+//                }
+//                else
+//                {
+//                    out.println("<h2>Erreur : Request Dispatcher = null ! </h2>");
+//                }
+//            }
+//            else
+//            {
+//                out.println("<h2>Erreur : Numéro de page incorrect ! </h2>");
+//            }
+        
+        if ( ressourceAInclure != null ) {
+        	// Stockage des objets à transmettre (dans attribut de requête)
+        	request.setAttribute("param", ressourceAInclure); 
+        	includeResource(ressourceAInclure, request, response);
+        }
+        
         //--- Pied de page
         footer ( out );
     }
---
+++ src/main/java/td/servlet/td7/Part1.java
@@ -0,0 +1,52 @@
+package td.servlet.td7 ;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * TD 7 
+ *  
+ * @author Laurent GUERIN 
+ * 
+ */
+@WebServlet(urlPatterns="/part1")
+public class Part1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	//--- Requete GET
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+					throws ServletException, IOException {
+		process(request, response);
+	}
+	//--- Requete POST 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+					throws ServletException, IOException {
+		process(request, response);
+	}
+	
+	private void process(HttpServletRequest request, HttpServletResponse response)
+					throws ServletException, IOException {
+	    String param = (String) request.getAttribute("param");
+	    
+	    ServletConfig config = getServletConfig();
+		PrintWriter out = response.getWriter();
+		out.println("<div style='background-color:yellow' >");
+		out.println("<h1>Part 1</h1>");
+		out.println("<h2> Param = " + param + "</h2> ");
+		out.println("<h2> ServletName = " + config.getServletName() + "</h2> ");
+		out.println("<br>");
+		out.println("<h4>Request URI = " + request.getRequestURI() + "</h4>");
+		out.println("<h4>Servlet Path = " + request.getServletPath() + "</h4>");
+		out.println("<h4>Path Info = " + request.getPathInfo() + "</h4>");
+		out.println("<h4>Path Translated = " + request.getPathTranslated() + "</h4>");
+		out.println("<h4>Protocol = " + request.getProtocol() + "</h4>");
+		out.println("</div>");
+	}
+}
